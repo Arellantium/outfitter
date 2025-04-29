47e3f34d85a91eceb50683ad59cea9a865f7d4f2
diodavi2804 <diodavi2804@gmail.com>
2025-04-29 09:58:28 +0200

    sistemazione file html

--- app/routers/post_foto.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, HTTPException, Depends, status
-from typing import List
-from pydantic import BaseModel
-
-app = FastAPI()
-
-class Post(BaseModel):
-    id : int
-    
