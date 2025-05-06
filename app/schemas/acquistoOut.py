50325996881f683def817dd2cc45d65740ebdff7
Alessandro Arellano <alessandroarellanoaltuna@gmail.com>
2025-05-06 12:15:23 +0200

    modifica acquisto.py

--- app/schemas/acquistoOut.py
+++ app/schemas/acquistoOut.py
@@ -2,16 +2,12 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class AcquistoPostOut(BaseModel):
+class AcquistoOut(BaseModel):
     id: int
     description: str
     author: str
     image_url: Optional[str]
-    created_at: str
-    likes: int
-    visualizzazioni: int
-    stato: str
-    visibile: bool
+    
 
     class Config:
         from_attributes = True
